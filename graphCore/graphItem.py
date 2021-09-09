daaafce9ea5e8192e6252a1dc1f4dffe68247d2d
meow <avs99.98@gmail.com>
2021-09-09 11:10:03 +0300

    create graph base clases

--- graphCore/graphItem.py
+++ graphCore/graphItem.py
@@ -5,12 +5,31 @@ class GraphItem:
         request- string
         ip- string
         """
-        self.request = ""
-        self.ip = ""
+        self.request = request
+        self.ip = ip
         self.next_items = []
         self.last_item = last_item
         self.percent = 0
         self.level = 0
 
+    def __init__(self, request, ip, last_item, level):
+        """
+        вызывается только из класса графа
+        request- string
+        ip- string
+        """
+        self.request = request
+        self.ip = ip
+        self.next_items = []
+        self.last_item = last_item
+        self.percent = 0
+        self.level = level
+
     def add_item(self, request, ip, last_item):
-        self.next_items.append(GraphItem(request, ip, last_item))
+        """
+        :param request: запрос
+        :param ip:
+        :param last_item: прошлый родительский элемент узла
+        :return: 
+        """
+        self.next_items.append(GraphItem(request, ip, last_item, self.level+1))
