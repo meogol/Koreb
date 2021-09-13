b846815293fabec2742034fccf1d6d8405ed7676
alex <avs99.98@gmail.com>
2021-09-13 22:23:41 +0300

    все починил

---
+++ ai/ns.py
@@ -0,0 +1,12 @@
+from keras import Sequential
+from keras.layers import GRU, Dense, Embedding, Flatten, Reshape, LSTM
+
+model = Sequential()
+model.add(Embedding(9, 128, input_length=100))
+model.add(LSTM(128, activation='tanh', return_sequences=True))
+model.add(LSTM(512, activation='tanh'))
+model.add(Dense(1000, activation='softmax'))
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
