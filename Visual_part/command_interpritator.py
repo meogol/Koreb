a2995e87cc3152c5121cdf600c8493366e35ec79
Evgenii <eugenefolkast@gmail.com>
2021-09-14 18:36:00 +0300

    maide base for neuro visual

--- Visual_part/command_interpritator.py
+++ Visual_part/command_interpritator.py
@@ -1,5 +1,5 @@
 from Visual_part.visual_make_ping import PingGenerator
-
+from Visual_part.visual import RunVisualisation
 
 class CommandInterpritator:
     def __init__(self):
@@ -7,27 +7,26 @@ class CommandInterpritator:
         self.ping = PingGenerator()
         self.x = 0
         self.y = 0
+        self.vis =RunVisualisation()
 
     def run_interpritation(self, command):
         if command is None:
             pass  # вывод ошибки на экран
         elif command == 'up':
-            self.x += 1
             self.y += 1
             self.ping.make_ping(self.x, self.y)
         elif command == 'down':
-            self.x -= 1
             self.y -= 1
             self.ping.make_ping(self.x, self.y)
         elif command == 'left':
-            self.y -= 1
+            self.x -= 1
             self.ping.make_ping(self.x, self.y)
         elif command == 'right':
-            self.y += 1
+            self.x += 1
             self.ping.make_ping(self.x, self.y)
         elif command == 'up-left':
-            self.x += 1
-            self.y -= 1
+            self.x -= 1
+            self.y += 1
             self.ping.make_ping(self.x, self.y)
         elif command == 'up-right':
             self.x += 1
@@ -38,8 +37,43 @@ class CommandInterpritator:
             self.y -= 1
             self.ping.make_ping(self.x, self.y)
         elif command == 'down-right':
-            self.x -= 1
-            self.y += 1
+            self.x += 1
+            self.y -= 1
             self.ping.make_ping(self.x, self.y)
         else:
             pass
+
+    def run_interpritation_neuro(self, command):
+        for item in command:
+            if item is None:
+                pass  # вывод ошибки на экран
+            elif item == 'up':
+                self.y += 1
+                self.vis.main_func(self.x, self.y)
+            elif item == 'down':
+                self.y -= 1
+                self.vis.main_func(self.x, self.y)
+            elif item == 'left':
+                self.x -= 1
+                self.vis.main_func(self.x, self.y)
+            elif item == 'right':
+                self.x += 1
+                self.vis.main_func(self.x, self.y)
+            elif item == 'up-left':
+                self.x -= 1
+                self.y += 1
+                self.vis.main_func(self.x, self.y)
+            elif item == 'up-right':
+                self.x += 1
+                self.y += 1
+                self.vis.main_func(self.x, self.y)
+            elif item == 'down-left':
+                self.x -= 1
+                self.y -= 1
+                self.vis.main_func(self.x, self.y)
+            elif item == 'down-right':
+                self.x += 1
+                self.y -= 1
+                self.vis.main_func(self.x, self.y)
+            else:
+                pass
