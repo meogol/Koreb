e509bbda2323676f82946afd9efabf57ac296f39
alex <avs99.98@gmail.com>
2021-09-27 11:02:51 +0300

    Auto stash before rebase of "origin/main"

---
+++ Taker/controller.py
@@ -0,0 +1,10 @@
+
+class Controller:
+    def __init__(self):
+        pass
+
+    def analyse_command(self, command):
+        pass
+
+    def update_graph(self):
+        pass
---
+++ Taker/graph_core/graph_generator.py
@@ -0,0 +1,15 @@
+class GraphGenerator:
+    def __init__(self):
+        self.graph = list()  # пока работаем с загдушкой в виде листа
+
+    def add_item(self):
+        pass
+
+    def search_item(self):
+        pass
+
+    def rebase_graph(self):
+        pass
+
+    def remove_item(self, command):
+        pass
