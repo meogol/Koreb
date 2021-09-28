44700c2a49e488bb1369b232a0b842ae8981d106
vanotuvanov <ivan.tumanov.1999@mail.ru>
2021-09-28 18:10:12 +0300

    Controller-Create the CacheItem class, Create the CommandCache class
    CacheItem was created, in the CommandCache process

---
+++ Controller/cash/cacheItem.py
@@ -0,0 +1,19 @@
+
+
+class CacheItem():
+    def __init__(self, v, i, c):
+        self.value = v
+        self.id = i
+        self.cacheItem = c
+
+    def get_value(self):
+        return self.value
+
+    def get_id(self):
+        return self.get_id
+
+    def get_cacheItem(self):
+        return self.cacheItem
+
+    def info(self):
+        return self.value, self.id, self.cacheItem
