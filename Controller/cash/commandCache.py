99eb6698b3cf65a16e9a18644efa03ca84bf07c6
vanotuvanov <ivan.tumanov.1999@mail.ru>
2021-09-29 16:12:40 +0300

    Auto stash before rebase of "main"

--- Controller/cash/commandCache.py
+++ Controller/cash/commandCache.py
@@ -2,22 +2,24 @@ import numbers
 
 from Controller.cash.cacheItem import CacheItem
 
+
 class CommandCache():
     def __init__(self):
         self.cache_active = list()
         self.predicted = list()
 
-    def append_to_cash(self):
+    def append_to_cash(self, i):
         self.cache_active.append()
 
-        def compare_lists(self):
-            pass
+    def compare_lists(self):
+        pass
 
-        def update_active(self):
-            pass
+    def update_active(self):
+        pass
 
     def check_lifeline(self):
         pass
+
 
 if __name__ == '__main__':
     rect = CacheItem(1, 2, 3)
