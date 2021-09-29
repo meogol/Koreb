2247762582c551fd67b0851d00112785d9c54766
ONEPANTSU <conandet@mail.ru>
2021-09-29 16:28:42 +0300

    Slice
    SREZ

--- Taker/api_core/test.py
+++ Taker/api_core/test.py
@@ -14,7 +14,6 @@ def test_request():
 
     traficGen = TrafficGenerator()
     ip, comToSend = traficGen.get_ip_and_command()
-    emptyIndex = dict()
     newComToSend = list()
     cortegeCount = 0
     i = 0
@@ -27,26 +26,14 @@ def test_request():
         if cortegeLen != 0:
             if i < len(comToSend):
                 if fill == 1:
-
-                    for j in range(cortegeLen):
-                        command += comToSend[i+j-cortegeLen]
-                    commands[cortegeCount] = command
+                    commands[cortegeCount] = comToSend[i-cortegeLen : i]
                     newComToSend.append(str(cortegeCount))
                     cortegeCount += 1
                 else:
-                    for j in range(cortegeLen):
-                        newComToSend.append("EMPTY")
-                        emptyIndex[i+j-cortegeLen] = len(newComToSend)-1
+                    newComToSend.append(comToSend[i-cortegeLen : i])
             else:
-                for j in range(len(comToSend)-(i-cortegeLen)):
-                    newComToSend.append("EMPTY")
-                    emptyIndex[i - cortegeLen + j] = len(newComToSend) - 1
-
-
-    keys = list(emptyIndex.keys())
-    for i in range(len(keys)):
-        newComToSend[emptyIndex.get(keys[i])] = comToSend[keys[i]]
-
+                newComToSend.append(comToSend[i-cortegeLen : len(comToSend)])
+                
     return commands, newComToSend
 
 if __name__ == '__main__':
