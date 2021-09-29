047d240c40924970821e25f5568c30f97cb7677c
alex <avs99.98@gmail.com>
2021-09-29 16:58:55 +0300

    Auto stash before rebase of "origin/main"

--- Controller/sniffer.py
+++ Controller/sniffer.py
@@ -10,22 +10,3 @@ class Sniffer:
     def get_ip_command(self):
         return self.traffgen.get_ip_and_command
 
-
-app = Flask(__name__)
-CORS(app)
-
-
-@app.route("/post_scene/", methods=['POST'])
-def post_scene():
-    """
-    принимает сценарий и передает на тейкер
-    """
-    scen = request.form.to_dict(flat=False)  # list
-
-    print(scen)
-
-    return "1"
-
-
-if __name__ == '__main__':
-    app.run()
