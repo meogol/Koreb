9e0bcb041ac45c9b9c0fd8db3d913d50282095c9
Danila <danila.matveev.2017@gmail.com>
2021-09-30 12:54:28 +0300

    update send_pakage method

--- Controller/send_data_to_taker.py
+++ Controller/send_data_to_taker.py
@@ -1,13 +1,20 @@
 import asyncio
+import time
+import requests
 
-class SendDataToTaker():
-    def send_pakage(self, list_of_commands):
 
+class SendDataToTaker:
+    def send_pakage(self, item):
+        ip = "15"
 
-        delay = 20*len(list_of_commands)
+        requests.post("http://127.0.0.1:5000/post_pkg/", data={'ip': ip, 'pkg': item})
+
+        delay = 20 * len(item)
         if delay > 2000:
             delay = 2000
-        await asyncio.sleep(delay)
+
+        time.sleep(delay/1000)
+        return 1
 
 
     def send_com_list_to_taker(self):
