a850407642a043784fcbcdfa275796942b9e2f0a
Evgenii <eugenefolkast@gmail.com>
2021-10-06 16:07:43 +0300

    Server v0.3

--- Server/server.py
+++ Server/server.py
@@ -5,27 +5,30 @@ from scapy.layers.inet import UDP
 from scapy.layers.inet import *
 from Controller.sniffer import Sniffer
 
+
 class Server():
 
     def __init__(self):
-        self.bytedata = []
-        self.sniffer = Sniffer ()
+        self.sniffer = Sniffer()
 
     def scan_pcapng_file(self):
         """
         Writes to bytedata all packages from pcap file in bytecode
         """
-        self.bytedata.clear()
-        self.bytedata = rdpcap("info.pcapng", count=10) #81188
+        bytedata = []
+        bytedata.clear()
+        bytedata = rdpcap("info.pcapng", count=10)  # 81188
 
-    def send_bytedata_to_sniffer(self):
+        for item in bytedata:
+            src.send_bytedata_to_sniffer(item)
+    def send_bytedata_to_sniffer(self, item):
         """
         Sends bytecode of one package to sniffer
         """
-        for item in self.bytedata:
+        while True:
             self.sniffer.send_command_to_controller(item)
+
 
 if __name__ == '__main__':
     src = Server()
     src.scan_pcapng_file()
-    src.send_bytedata_to_sniffer()
