8458fa807d5f73d5019af155e7892934b567ce58
alex <avs99.98@gmail.com>
2021-10-07 18:17:18 +0300

    upd

--- start.py
+++ start.py
@@ -1,5 +1,4 @@
 from threading import Thread
-from time import sleep
 
 from Taker.api_core import api as api
 from Server.server import Server
@@ -9,12 +8,11 @@ class StartProgram():
     def __init__(self):
         self.api = api
         self.server = Server()
-        th = Thread(target=self.server.run)
+        th = Thread(target=api.run)
         th.start()
 
     def lets_go(self):
-        self.api.run()
-
+        self.server.run()
 
 if __name__ == '__main__':
     start = StartProgram()
