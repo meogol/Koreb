2a6be940521918604228d2c7715eeaa532bb595c
ONEPANTSU <conandet@mail.ru>
2021-10-07 13:13:47 +0300

    Client test

---
+++ Client/client_test.py
@@ -0,0 +1,24 @@
+import requests
+
+from Controller.traffic_generator.trafficGenerator import TrafficGenerator
+
+
+
+def send_server_pakage(pkg, ip="15"):
+    r = requests.post("http://127.0.0.1:5000/post_server_pkg/", data={'ip': ip, 'pkg': pkg})
+    print(r)
+    return 1
+
+
+def send_taker_pakage(pkg, ip="15"):
+    r = requests.post("http://127.0.0.1:5000/post_taker_pkg/", data={'ip': ip, 'pkg': pkg})
+    print(r)
+    return 1
+
+
+if __name__ == '__main__':
+
+    trafficGen = TrafficGenerator()
+    ip, traffic = trafficGen.get_ip_and_command()
+    send_server_pakage(traffic)
+    send_taker_pakage(traffic)
