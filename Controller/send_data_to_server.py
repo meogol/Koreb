ac9da8a4b9a6392aacd4dd9e4f7853e1f35f6ad9
Evgenii <eugenefolkast@gmail.com>
2021-10-11 13:11:31 +0300

    v1.1

--- Controller/send_data_to_server.py
+++ Controller/send_data_to_server.py
@@ -6,7 +6,7 @@ class SendDataToServer:
         """
         Sends status to the server
         """
-        requests.post(" http://127.0.0.1:5000/logging/", data={status:status})
+        requests.post(" http://127.0.0.1:5000/logging/", data={"status":status})
 
         return 1
 
