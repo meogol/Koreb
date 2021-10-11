675933587d0a2a264c7efefa4ba6b96c47ad4853
Evgenii <eugenefolkast@gmail.com>
2021-10-11 12:39:52 +0300

    Server api v1.0

--- Server/api.py
+++ Server/api.py
@@ -1,18 +1,31 @@
 from flask import Flask, request
 from flask_cors import CORS
 
-
 app = Flask(__name__)
 CORS(app)
 
 
-@app.route("/trouble_message/", methods=['POST'])
-def trouble_message():
+@app.route("/logging/", methods=['POST'])
+def logging():
     """
     Get an error and displaying it into logs
     """
-    error_message = request.form
-    app.logger.error("Error received: " + error_message)
+    message = request.form.to_dict(flat=False)
+    error = message["error"] #str
+
+    if error == 100: #process corrupted error
+        app.logger.error("The error 100 occurred: "
+                         "process corrupted")
+    elif error == 404: #command lost error
+        app.logger.error("The error 404 occurred:"
+                         "commands lost ")
+    elif error == 200: #Client is working
+        app.logger.error("The error 200 occurred: "
+                         "Client is working right now")
+    elif error == 303: #Client doesn't responding
+        app.logger.error("The error 303 occurred:"
+                         "Client doesn't responding")
+
 
     return 1
 
