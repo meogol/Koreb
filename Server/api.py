82c8a75abda0a373f220ffc0d092f303ccc045c2
Evgenii <eugenefolkast@gmail.com>
2021-10-11 13:06:46 +0300

    Controller server api v1.0

--- Server/api.py
+++ Server/api.py
@@ -11,23 +11,25 @@ def logging():
     Get an error and displaying it into logs
     """
     message = request.form.to_dict(flat=False)
-    error = message["error"] #str
+    status = message["status"] #str
 
-    if error == 100: #process corrupted error
+    if status == 100: #process corrupted error
         app.logger.error("The error 100 occurred: "
                          "process corrupted")
-    elif error == 404: #command lost error
+    elif status == 404: #command lost error
         app.logger.error("The error 404 occurred:"
                          "commands lost ")
-    elif error == 200: #Client is working
+    elif status == 200: #Client is working
         app.logger.error("The error 200 occurred: "
                          "Client is working right now")
-    elif error == 303: #Client doesn't responding
+    elif status == 303: #Client doesn't responding
         app.logger.error("The error 303 occurred:"
                          "Client doesn't responding")
-
+    else: return app.logger.success("Commands processing successful")
 
     return 1
 
+if __name__ == '__main__':
+    app.run()
 
 
