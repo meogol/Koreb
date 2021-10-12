e58e523bc1c23ff1b6c1b8a56ab43c7aa5f6d3ac
ONEPANTSU <conandet@mail.ru>
2021-10-12 12:26:10 +0300

    Auto stash before rebase of "origin/main"

--- start.py
+++ start.py
@@ -1,9 +1,15 @@
 from threading import Thread
-
 from Taker.api_core import api as api
 from Server.server import Server
+<<<<<<< Updated upstream:start.py
 from Controller.get_respond_from_taker import run as respond_run
 
+=======
+
+"""
+"Точка входа"
+"""
+>>>>>>> Stashed changes:Start/start.py
 class StartProgram():
     def __init__(self):
         self.api = api
@@ -13,10 +19,16 @@ class StartProgram():
 
     def lets_go(self):
         """
+<<<<<<< Updated upstream:start.py
         Entry to the programm
         """
         th = Thread(target=respond_run())
         th.start()
+=======
+        Запуск сервера
+        """
+        self.server.run()
+>>>>>>> Stashed changes:Start/start.py
 
 if __name__ == '__main__':
     start = StartProgram()
