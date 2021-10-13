b63c276fd1cbee8fd44047a20334217a000bf8d7
Evgenii <eugenefolkast@gmail.com>
2021-10-13 11:04:42 +0300

    Controller server v 1.4

--- start.py
+++ start.py
@@ -1,25 +1,34 @@
+import time
 from threading import Thread
 
-from Taker.api_core import api as api
+from Taker.api_core import server_api as api
 from Server.server import Server
 from Controller.get_respond_from_taker import run as respond_run
-from Server.api import run as server_api_run
+from Server import api as server_api
+from time import sleep
+
 
 class StartProgram():
     def __init__(self):
         self.api = api
         self.server = Server()
-        th = Thread(target=api.run)
-        th.start()
-        lh = Thread(target=server_api_run)
-        lh.start()
-        nh = Thread(target=respond_run)
-        nh.start()
+        self.server_api_runner = server_api
+
+
+        # nh = Thread(target=respond_run)
+        # nh.start()
 
     def lets_go(self):
         """
-        Entry to the programm
+        Entry to the program
         """
+        lh = Thread(target=server_api.run)
+        lh.start()
+
+        sleep(2)
+
+        th = Thread(target=api.run)
+        th.start()
         self.server.run()
 
 if __name__ == '__main__':
