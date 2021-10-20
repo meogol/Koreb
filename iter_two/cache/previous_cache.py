3747b5425ac343fd172c6a57a36e60180c797d0a
ONEPANTSU <conandet@mail.ru>
2021-10-20 16:57:04 +0300

    Auto stash before rebase of "origin/main"
    Previous_Cache

---
+++ iter_two/cache/previous_cache.py
@@ -0,0 +1,26 @@
+
+class PreviousCache:
+
+    def __init__(self, dict):
+        self.previous_dict = dict.copy
+
+    def __init__(self):
+        self.previous_dict = {'IP':None, 'DATA':None}
+
+    def __str__(self):
+        return str(self.previous_dict.items())
+
+    def update_previous_cache(self, dict):
+        self.previous_dict.clear()
+        self.previous_dict.update(dict)
+
+    def get_previous_cache(self):
+        return self.previous_dict
+
+
+
+if __name__ == '__main__':
+    cache = PreviousCache()
+    print(cache)
+    cache.update_previous_cache({('12.12.12.12', 'do smthng, u!'), ('13.13.13.13', 'oh...')})
+    print(cache)
