acc1b497672ccba31999caaf94850b74c446d943
vanotuvanov <ivan.tumanov.1999@mail.ru>
2021-10-21 17:43:59 +0300

    create class Agregation_cache
    the class contains methods for adding and returning the cache

---
+++ iter_two/controller/aggregation_cache.py
@@ -0,0 +1,30 @@
+
+class Agregation_cache:
+    def __init__(self):
+        self.cache = [0]*10
+
+
+    def add_cache(self, commands):
+        self.cache.insert(0, commands)
+        del self.cache[10]
+
+    """получение элемента кэша"""
+    def receiving_cache(self, number):
+        return self.cache[number-1]
+
+if __name__ == '__main__':
+    add = Agregation_cache()
+    add.add_cache([12, 3, 40])
+    add.add_cache([37, 80, 214])
+    add.add_cache([5, 90, 43])
+    add.add_cache([654, 234, 4435])
+    add.add_cache([782, 578, 354])
+    add.add_cache([531, 786, 621])
+    add.add_cache([513, 45, 972])
+    add.add_cache([6452, 215, 60])
+    add.add_cache([943, 45, 267])
+    add.add_cache([563, 542, 228])
+    add.add_cache([372, 6523, 389])
+    add.add_cache([267, 821, 26])
+    add.add_cache([687, 87, 65])
+    print(add.receiving_cache(7))
