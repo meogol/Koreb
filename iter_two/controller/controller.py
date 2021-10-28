33ae5aa1d54cfedce01e1867e9379ea4f1532186
Danila <danila.matveev.2017@gmail.com>
2021-10-28 10:39:16 +0300

    upd

--- iter_two/controller/controller.py
+++ iter_two/controller/controller.py
@@ -11,6 +11,5 @@ class Controller:
 
     def analyse_command(self, package):
         res = self.aggregator.contrast_last_package(package)
-        self.server.send_package(package)
         self.cache_manager.add_agr_cache(1, package)
-
+        self.server.send_package(package)
