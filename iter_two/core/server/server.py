e93f436286a42d978270879e74247575a571ad9d
alex <avs99.98@gmail.com>
2021-11-16 17:57:59 +0300

    ss

--- iter_two/core/server/server.py
+++ iter_two/core/server/server.py
@@ -1,14 +1,14 @@
 import threading
 
 from iter_two.core.server.serv_socket_client import SocketClient
-from iter_two.core.server.serv_socket_server import SocketListener
+from iter_two.core.server.serv_socket_server import SocketServer
 from setting_reader import setting_res
 
 
 class Server:
     def __init__(self, host='localhost', port=7777):
-        # self.socket_client = SocketClient(host=str(setting_res.get('host')), port=int(setting_res.get('port')))
-        self.socket_server = SocketListener(host=str(setting_res.get('host')), port=int(setting_res.get('port')))
+        self.socket_client = SocketClient(host=str(setting_res.get('host')), port=int(setting_res.get('port')))
+        self.socket_server = SocketServer(host=str(setting_res.get('host')), port=int(setting_res.get('port')))
 
     def init_listener_thread(self):
         t = threading.Thread(target=self.socket_server.run_listener_server)
