11ffc0463ea3cbb4344e81b60cb7e5bffee261ce
alex <avs99.98@gmail.com>
2021-11-16 17:57:18 +0300

    s

--- iter_two/core/server/serv_socket_client.py
+++ iter_two/core/server/serv_socket_client.py
@@ -8,7 +8,7 @@ from iter_two.taker.taker import Taker
 
 
 class SocketClient:
-    def __init__(self, host='192.168.0.101', port=7788):
+    def __init__(self, host='192.168.0.101', port=7777):
         self.host = host
         self.port = port
         self.client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -20,17 +20,13 @@ class SocketClient:
         @param: resending: отправляется ли пакет повторно
         """
         while 1:
-            send_msg = input("Ты сказал:")
+            send_msg = str(package)
+            send_msg = send_msg.replace("[", "[" + destination_ip + ", ", 1)
             # Используйте этот сокет для кодирования того, что вы вводите, и отправьте его на этот адрес и
             # соответствующий порт
             self.client.sendto(send_msg.encode('utf-8'), (self.host, self.port))
-            if send_msg == 'q':
-                break
+
             # Декодировать полученную информацию
             back_msg = self.client.recv(1024).decode('utf-8')
-            # Печать декодированной информации
-            print(back_msg)
 
         self.client.close()
-
-
--- iter_two/core/server/serv_socket_server.py
+++ iter_two/core/server/serv_socket_server.py
@@ -7,31 +7,32 @@ import socket
 from iter_two.taker.taker import Taker
 
 
-class SocketListener:
+class SocketServer:
     def __init__(self, host='192.168.0.101', port=7777):
         self.host = host
         self.port = port
         self.fwq = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.fwq.bind((host, port))
 
+        self.taker = Taker()
+
     def run_listener_server(self):
         while 1:
             # Получать сообщения и адреса. Recvfrom используется для получения сообщений в UDP
-            data, addr = self.fwq.recvfrom(1024)
+            data, addr = self.fwq.recvfrom(10240000)
             # Декодировать полученное сообщение
             recvmsg = data.decode('utf-8')
-            # Условия завершения цикла while
-            if recvmsg == 'q':
-                print("Другой участник добровольно закончил чат с тобой, пока!")
-                break
-            # Распечатать декодированное сообщение
-            print('client msg:' + recvmsg)
-            replymsg = input('Ответить:')
-            # Отправка сообщения, на которое вы хотите ответить, отправка полученному адресу, отправка в UDP также
-            # использует sendto
+            self.taker.start(recvmsg)
+
+            replymsg = '2'
             self.fwq.sendto(replymsg.encode('utf-8'), addr)
 
         self.fwq.close()
 
     def data_listener(self, d):
         pass
+
+
+if __name__ == '__main__':
+    s = SocketServer()
+    s.run_listener_server()
