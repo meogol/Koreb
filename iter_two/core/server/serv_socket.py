d05390e0f015163894b4a934548ed57bd96ddc11
ONEPANTSU <conandet@mail.ru>
2021-11-16 15:42:00 +0300

    Socket change 2

--- iter_two/core/server/serv_socket.py
+++ iter_two/core/server/serv_socket.py
@@ -39,7 +39,7 @@ class SocketClient:
 
     def send_package(self):
         for item in self.cache_socket:
-            self.socket.sendto(item.encode('utf-8'), self.__controller_addr)
+            self.socket.sendto(item.encode('utf-8'), self.__taker_addr)
 
         d = self.socket.recvfrom(10240000000)
         reply = d[0]
