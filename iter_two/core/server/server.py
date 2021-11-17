d1d64f56e4ec6dfece17f15e8522907e31577bf4
ONEPANTSU <conandet@mail.ru>
2021-11-17 16:46:28 +0300

    Final Fix

--- iter_two/core/server/server.py
+++ iter_two/core/server/server.py
@@ -7,20 +7,16 @@ from setting_reader import setting_res
 
 
 class Server:
-    def __init__(self, socket_type="server", host=str(setting_res.get('host')), port=int(setting_res.get('port')), ip=str(setting_res.get('taker_ip'))):
+    def __init__(self, socket_type="server", host=str(setting_res.get('host')), port=int(setting_res.get('port'))):
         """
         socket_type = "server" or "client"
         """
         self.socket_type = socket_type
-        if self.socket_type == "server":
-            self.ip = ip
-        else:
-            self.ip = host
 
         if self.socket_type == "server":
-            self.socket = SocketServer(host=self.ip, port=port)
+            self.socket = SocketServer(host=host, port=port)
         else:
-            self.socket = SocketClient(host=self.ip, port=port)
+            self.socket = SocketClient(host=host, port=port)
 
     def init_listener(self):
         self.socket.run_listener_server()
--- startTaker.py
+++ startTaker.py
@@ -4,7 +4,7 @@ from setting_reader import setting_read, setting_res
 if __name__ == '__main__':
     setting_read()
 
-    server = Server(socket_type="server", host=str(setting_res.get('host')), port=int(setting_res.get('port')), ip=str(setting_res.get('taker_ip')))
+    server = Server(socket_type="server", host=str(setting_res.get('host')), port=int(setting_res.get('port')))
     server.init_listener()
 
     input("End")
