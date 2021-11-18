74377b1f8d12b4a680e1e1ec79cfe703961c158d
ONEPANTSU <conandet@mail.ru>
2021-11-18 12:34:34 +0300

    FINAL TAKER!!!

--- iter_two/taker/taker.py
+++ iter_two/taker/taker.py
@@ -2,6 +2,8 @@ import numpy as np
 from scapy.all import *
 from scapy.layers.inet import ICMP, TCP
 from scapy.layers.ipsec import IP
+from scapy.layers.l2 import Ether
+
 from iter_two.printer import print_len
 from iter_two.core.cahce.cache import CacheManager
 
@@ -22,23 +24,26 @@ class Taker:
         destination_ip = list_bytes[0].replace("\'", "")  # ip получателя пакета
         list_bytes = list(map(int, list_bytes[1:]))
 
-        if self.cache_manager.get_last_pkg_cache(destination_ip) is None:
-            self.cache_manager.add_last_pkg_cache(destination_ip, list_bytes)
-            list_to_send = bytes(list_bytes)
-            # self.to_send(destination_ip, list_to_send)
+        if ':' not in destination_ip:
 
-            return
+            if self.cache_manager.get_last_pkg_cache(destination_ip) is None:
+                self.cache_manager.add_last_pkg_cache(destination_ip, list_bytes)
+                list_to_send = bytes(list_bytes)
+                self.to_send(destination_ip, list_to_send)
 
-        last_pkg = self.cache_manager.get_last_pkg_cache(destination_ip)
-        res = self.recovery_pkg(list_bytes, last_pkg)
+                return
 
-        print_len(msg="\nAgregate length:\t", pkg=list_bytes, dst=destination_ip, print_pkg=False)
-        print_len(msg="Resource length:\t", pkg=res, dst=destination_ip, print_pkg=False)
+            last_pkg = self.cache_manager.get_last_pkg_cache(destination_ip)
+            res = self.recovery_pkg(list_bytes, last_pkg)
 
-        self.cache_manager.add_all_cache(destination_ip, res)
+            print_len(msg="\nAgregate length:\t", pkg=list_bytes, dst=destination_ip, print_pkg=False)
+            print_len(msg="Resource length:\t", pkg=res, dst=destination_ip, print_pkg=False)
 
-        list_to_send = bytes(res)
-        # self.to_send(destination_ip, list_to_send)
+            self.cache_manager.add_all_cache(destination_ip, res)
+
+            list_to_send = bytes(res)
+            self.to_send(destination_ip, list_to_send)
+
 
     def recovery_pkg(self, package, last_pkg):
         """
@@ -75,7 +80,10 @@ class Taker:
 
     def to_send(self, dst_ip, package, port=7777):
         package = bytes(package)
-        pkt = IP(dst=dst_ip)/TCP(dport=port)/Raw(package)
+        if ':' not in dst_ip:
+            pkt = IP(dst=dst_ip)/TCP(dport=port)/Raw(package)
+        else:
+            pkt = Ether(dst=dst_ip) / TCP(dport=port) / Raw(package)
         send(pkt)
 
 
