83f8982c96c8f6fc278f77b7551d52823f10875b
alex <avs99.98@gmail.com>
2021-11-25 10:16:29 +0300

    working sniff

--- startController.py
+++ startController.py
@@ -1,10 +1,9 @@
-from iter_two.core.snif.sniffer import Sniffer
+
+from iter_two.core.snif.spoofer import Spoofer
 from setting_reader import setting_read, setting_res
 
 if __name__ == '__main__':
     setting_read()
 
-    sniffer = Sniffer()
-
-    sniffer.traff_file_read(pkg=setting_res.get("pkg_type"), ip=setting_res.get("ip"), port=setting_res.get("port"))
-
+    spoofer = Spoofer()
+    spoofer.to_process()
