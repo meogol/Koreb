0144991888b76962219215c3e1049afea8f48ef2
ONEPANTSU <conandet@mail.ru>
2021-12-09 17:37:02 +0300

    Updated Aggr

--- iter_two/controller/aggregator.py
+++ iter_two/controller/aggregator.py
@@ -1,14 +1,11 @@
 import numpy as np
 
-from iter_two.core.cahce.cache import CacheManager
-from iter_two.taker.taker import Taker
-
-
 class Aggregator:
     def __init__(self, cache_manager):
         self.cache_manager = cache_manager
 
     def contrast_last_package(self, package, destination_ip):
+
         if self.cache_manager.get_last_pkg_cache(destination_ip) is not None:
             last_pkg = np.array(self.cache_manager.get_last_pkg_cache(destination_ip))
             this_pkg = np.array(package)
@@ -16,42 +13,33 @@ class Aggregator:
             this_pkg_len = len(package)
             tail = []
 
-            if last_pkg_len == this_pkg_len:
-                diff = abs(np.subtract(last_pkg, this_pkg))
-            elif last_pkg_len > this_pkg_len:
+
+            if last_pkg_len > this_pkg_len:
                 last_pkg = last_pkg[:this_pkg_len]
-                diff = abs(np.subtract(last_pkg, this_pkg))
-            else:
+
+            elif last_pkg_len < this_pkg_len:
                 tail = this_pkg[last_pkg_len:]
                 this_pkg = this_pkg[:last_pkg_len]
-                diff = abs(np.subtract(last_pkg, this_pkg))
 
-            if np.nonzero(diff)[0].size == len(diff):
-                if len(tail) != 0:
-                    return list(this_pkg) + list(tail)
-                else:
-                    return list(this_pkg)
-            elif np.nonzero(diff)[0].size == 0:
+            diff = abs(np.subtract(last_pkg, this_pkg))
+
+
+
+            if np.nonzero(diff)[0].size == 0:
                 this_pkg = [-len(diff)]
-                if len(tail) != 0:
-                    return list(this_pkg) + list(tail)
-                else:
-                    return list(this_pkg)
-            else:
+
+            elif np.nonzero(diff)[0].size != len(diff):
                 nz = np.nonzero(diff)[0]
                 prev = None
                 slices = []
                 if nz[0] != 0:
                     slices.append([0, nz[0]])
+                    
                 for i in nz:
-                    if prev is None:
-                        prev = i
-                    else:
-                        if i - prev >= 2:
-                            slices.append([prev + 1, i])
-                            prev = i
-                        else:
-                            prev = i
+                    if prev is not None and i - prev >= 2:
+                        slices.append([prev + 1, i])
+                    prev = i
+
                 if nz[-1] + 1 != len(diff):
                     slices.append([nz[-1] + 1, len(diff)])
 
@@ -62,7 +50,7 @@ class Aggregator:
                     this_pkg[i[0] - shift:i[1] - shift] = [-(i[1] - i[0])]
                     shift += (i[1] - i[0] - 1)
 
-                if len(tail) != 0:
-                    return list(this_pkg) + list(tail)
-                else:
-                    return list(this_pkg)
+            if len(tail) != 0:
+                return list(this_pkg) + list(tail)
+            else:
+                return list(this_pkg)
