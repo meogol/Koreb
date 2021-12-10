405f07128bc5e64c1033a9ede0bcdb30baff9485
vanotuvanov <ivan.tumanov.1999@mail.ru>
2021-12-10 14:49:51 +0300

    Update test_agregator

--- iter_two/test/test_aggregator.py
+++ iter_two/test/test_aggregator.py
@@ -2,21 +2,20 @@ from iter_two.controller.aggregator import Aggregator
 from iter_two.core.cahce.cache import CacheManager
 from iter_two.taker.taker import Taker
 
-
 def method(last, this):
     cache = CacheManager()
     taker = Taker()
     Last = last
     This = this
-    cache.add_all_cache("192.168.0.106", Last)
+    cache.add_all_cache(ip="192.168.0.106", package=Last)
     # добавление предыдущего пакета в кэш
     agr = Aggregator(cache)
-    agr_this = agr.contrast_last_package(this, "192.168.0.106")
+    agr_this = agr.contrast_last_package(package=this, destination_ip="192.168.0.106")
     # агрегация текущего пакета
     print("Изначальный:", This)
     print("Предыдущий:", Last)
     print("Агрегированный:", agr_this)
-    ras_pack2 = taker.recovery_pkg(agr_this, Last)
+    ras_pack2 = taker.recovery_pkg(package=agr_this, last_pkg=Last)
     # распаковка текущего пакета
     ras = ras_pack2.tolist()
     # приведение распакованного текущего пакета к списку
@@ -26,7 +25,6 @@ def method(last, this):
     else:
         return 0
 
-
 # @pytest.mark.agreg_same
 def test_contrast_last_package1():
     # в данном тесте сравнение пакетов с одинаковой длинной
@@ -35,7 +33,6 @@ def test_contrast_last_package1():
     a = (method(last, this))
     assert a == 1
 
-
 # @pytest.mark.agreg_large_this
 def test_contrast_last_package2():
     # в данном тесте сравнение пакетов, когда предыдущий короче текущего
@@ -43,7 +40,6 @@ def test_contrast_last_package2():
     this = [20, 12, 123, 35, 0, 0, 40, 5, 3, 5, 7, 3, 2, 67, 243, 34, 26, 87, 186, 12, 42, 534]
     a = (method(last, this))
     assert a == 1
-
 
 # @pytest.mark.agreg_smal_this
 def test_contrast_last_package3():
