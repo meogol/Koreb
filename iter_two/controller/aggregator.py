4de7b2a7e4a63a93b8db8964428f1391b724f1d7
ONEPANTSU <conandet@mail.ru>
2021-12-13 15:08:05 +0300

    Updated Aggr 2.0

--- iter_two/controller/aggregator.py
+++ iter_two/controller/aggregator.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy
 
 class Aggregator:
     def __init__(self, cache_manager):
@@ -7,12 +7,11 @@ class Aggregator:
     def contrast_last_package(self, package, destination_ip):
 
         if self.cache_manager.get_last_pkg_cache(destination_ip) is not None:
-            last_pkg = np.array(self.cache_manager.get_last_pkg_cache(destination_ip))
-            this_pkg = np.array(package)
+            last_pkg = numpy.array(self.cache_manager.get_last_pkg_cache(destination_ip))
+            this_pkg = numpy.array(package)
             last_pkg_len = len(last_pkg)
             this_pkg_len = len(package)
             tail = []
-
 
             if last_pkg_len > this_pkg_len:
                 last_pkg = last_pkg[:this_pkg_len]
@@ -21,34 +20,33 @@ class Aggregator:
                 tail = this_pkg[last_pkg_len:]
                 this_pkg = this_pkg[:last_pkg_len]
 
-            diff = abs(np.subtract(last_pkg, this_pkg))
+            diff = numpy.subtract(last_pkg, this_pkg)
 
+            nonzero_size = numpy.nonzero(diff)[0].size
 
-
-            if np.nonzero(diff)[0].size == 0:
+            if nonzero_size == 0:
                 this_pkg = [-len(diff)]
 
-            elif np.nonzero(diff)[0].size != len(diff):
-                nz = np.nonzero(diff)[0]
+            elif nonzero_size != len(diff):
+                nonzero = numpy.nonzero(diff)[0]
                 prev = None
-                slices = []
-                if nz[0] != 0:
-                    slices.append([0, nz[0]])
-                    
-                for i in nz:
-                    if prev is not None and i - prev >= 2:
-                        slices.append([prev + 1, i])
-                    prev = i
-
-                if nz[-1] + 1 != len(diff):
-                    slices.append([nz[-1] + 1, len(diff)])
 
                 this_pkg = list(this_pkg)
                 shift = 0
 
-                for i in slices:
-                    this_pkg[i[0] - shift:i[1] - shift] = [-(i[1] - i[0])]
-                    shift += (i[1] - i[0] - 1)
+                if nonzero[0] != 0:
+                    this_pkg[0 - shift:nonzero[0] - shift] = [- nonzero[0]]
+                    shift += (nonzero[0] - 1)
+
+                for i in nonzero:
+                    if prev is not None and i - prev >= 2:
+                        this_pkg[prev + 1 - shift:i - shift] = [- i + prev + 1]
+                        shift += (i - prev - 2)
+                    prev = i
+
+                if nonzero[-1] + 1 != len(diff):
+                    this_pkg[nonzero[-1] + 1 - shift:len(diff) - shift] = [- len(diff) + nonzero[-1] + 1]
+                    shift += (len(diff) - nonzero[-1] - 2)
 
             if len(tail) != 0:
                 return list(this_pkg) + list(tail)
