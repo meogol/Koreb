9896f4e32e369294e2aef45aa72ccab228faa3de
vanotuvanov <ivan.tumanov.1999@mail.ru>
2021-12-22 14:08:17 +0300

    BUG

--- iter_two/taker/taker.py
+++ iter_two/taker/taker.py
@@ -24,15 +24,19 @@ class Taker:
         @return:
         """
         int_list = pickle.loads(package)
+        print("int_list\t" + str(int_list))
 
-        if self.cache_manager.get_last_pkg_cache('192.168.0.101') is not None:
-            int_list = Taker.recovery_pkg(int_list, self.cache_manager.get_last_pkg_cache('192.168.0.101'))
-        self.cache_manager.add_all_cache('192.168.0.101', int_list)
+        if self.cache_manager.get_last_pkg_cache('192.168.1.57') is not None:
+            int_list = Taker.recovery_pkg(int_list, self.cache_manager.get_last_pkg_cache('192.168.1.57'))
+
+        self.cache_manager.add_all_cache('192.168.1.57', int_list)
+
+        if type(int_list) == numpy.ndarray:
+            int_list = int_list.tolist()
 
         int_package = 0
         for i in range(len(int_list)):
             int_package += int_list[i] * 10 ** (len(int_list) - i - 1)
-        print("int_list\t" + str(int_list))
         print("int_package\t" + str(int_package))
 
         byte_package = int_to_bytes(int_package)
@@ -48,32 +52,29 @@ class Taker:
         @param package: передаваемый пакет. Передавать стоит в виде листа чисел
         @return: восстановленный пакет. Возвращается в виде листа чисел
         """
-        try:
-            filtered = list()
-            for item in package:
-                if item >= 0:
-                    filtered.append(item)
-                else:
-                    filtered.extend([-1] * -item)
+        filtered = list()
+        for item in package:
+            if item >= 0:
+                filtered.append(item)
+            else:
+                filtered.extend([-1] * -item)
 
-            this_pkg = np.array(filtered)
-            last_pkg = np.array(last_pkg)
+        this_pkg = np.array(filtered)
+        last_pkg = np.array(last_pkg)
 
-            tail = []
-            if len(this_pkg) > len(last_pkg):
-                tail = this_pkg[len(last_pkg):]
-                this_pkg = this_pkg[:len(last_pkg)]
-            elif len(this_pkg) < len(last_pkg):
-                last_pkg = last_pkg[:len(this_pkg)]
+        tail = []
+        if len(this_pkg) > len(last_pkg):
+            tail = this_pkg[len(last_pkg):]
+            this_pkg = this_pkg[:len(last_pkg)]
+        elif len(this_pkg) < len(last_pkg):
+            last_pkg = last_pkg[:len(this_pkg)]
 
-            new_pkg = np.where(this_pkg >= 0, this_pkg, last_pkg)
+        new_pkg = np.where(this_pkg > 0, this_pkg, last_pkg)
 
-            if len(tail) != 0:
-                new_pkg = np.append(new_pkg, tail)
-
-            return new_pkg
-        except TypeError:
-            print("RECOVERY ERROR!")
+        if len(tail) != 0:
+            new_pkg = np.append(new_pkg, tail)
+        print("new pkg:", new_pkg)
+        return new_pkg
 
 
 def int_to_bytes(x: int) -> bytes:
